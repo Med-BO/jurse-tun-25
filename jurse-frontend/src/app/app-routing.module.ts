61c03595ebd7e62e3203ac2d1a248d231a5abddb
Mohamed Ben Othman <Mohamedbenothman2@outlook.com>
2024-01-06 20:00:11 +0100

    Added the routing and the base screens components

--- jurse-frontend/src/app/app-routing.module.ts
+++ jurse-frontend/src/app/app-routing.module.ts
@@ -1,7 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainLayoutComponent } from './pages/main-layout/main-layout.component';
+import { HomeComponent } from './pages/home/home.component';
+import { DatesComponent } from './pages/dates/dates.component';
+import { ParticipationComponent } from './pages/participation/participation.component';
+import { PresentationComponent } from './pages/presentation/presentation.component';
+import { CommiteesComponent } from './pages/commitees/commitees.component';
+import { SponsorsComponent } from './pages/sponsors/sponsors.component';
+import { VenueComponent } from './pages/venue/venue.component';
+import { MediaComponent } from './pages/media/media.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "jurse", component: MainLayoutComponent, children: [
+      { path: "home", component: HomeComponent },
+      { path: "dates", component: DatesComponent },
+      { path: "participation", component: ParticipationComponent },
+      { path: "presentation", component: PresentationComponent },
+      { path: "commitees", component: CommiteesComponent },
+      { path: "sponsors", component: SponsorsComponent },
+      { path: "venue", component: VenueComponent },
+      { path: "media", component: MediaComponent },
+      { path: "contact", component: ContactComponent },
+    ] 
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- jurse-frontend/src/app/app.module.ts
+++ jurse-frontend/src/app/app.module.ts
@@ -3,14 +3,38 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainLayoutComponent } from './pages/main-layout/main-layout.component';
+import { HomeComponent } from './pages/home/home.component';
+import { DatesComponent } from './pages/dates/dates.component';
+import { ParticipationComponent } from './pages/participation/participation.component';
+import { PresentationComponent } from './pages/presentation/presentation.component';
+import { CommiteesComponent } from './pages/commitees/commitees.component';
+import { SponsorsComponent } from './pages/sponsors/sponsors.component';
+import { VenueComponent } from './pages/venue/venue.component';
+import { MediaComponent } from './pages/media/media.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainLayoutComponent,
+    HomeComponent,
+    DatesComponent,
+    ParticipationComponent,
+    PresentationComponent,
+    CommiteesComponent,
+    SponsorsComponent,
+    VenueComponent,
+    MediaComponent,
+    ContactComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
